df1669c5ac472858cf989d3a79787256b6918cc6
xavier21210 <117159696+xavier21210@users.noreply.github.com>
2024-03-27 17:00:13 +0100

    fisrt commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Header from "./components/Header.svelte";
+  import Home from "./components/Home.svelte";
+  import Footer from "./components/Footer.svelte";
+  let routes = {
+    // define your routes here
+    "/": Header,
+  "/home": Home,	
+     "/footer": Footer,
+    // '/other': OtherComponent,
+  };
+</script>
+
+
+<Header/>
+
+  <body>
+	<Home/>
+  
+  </body>
+
+<Footer/>
+
---
+++ src/components/Header.svelte
@@ -0,0 +1,38 @@
+<script>
+
+</script>
+
+<div class="containerUp">
+    <h2>
+       <a href="#menu">Click for beauty</a>
+    </h2>
+       
+   <div class="popover" id="menu">
+    <div class = 'content'>
+     <a href="#" class="close"></a>
+      <div class = 'nav'>
+       <ul class = 'nav_list'>
+         
+         <div class = 'nav_list_item'>
+         <li><a href="#">Home</a></li>
+         </div>
+        <div class = 'nav_list_item'>
+           <li><a href="#">About</a></li>
+         </div>
+       <div class = 'nav_list_item'>
+         <li><a href="#">Products</a></li>
+         </div>
+       <div class = 'nav_list_item'>
+         <li><a href="#">Services</a></li>
+         </div>
+        <div class = 'nav_list_item'>
+           <li><a href="#">Contact</a></li>
+         </div>
+         
+       </ul>
+     </div>
+    </div>
+    </div>
+    </div>
+
+    
