e7e9c87f5964a8d2c50a3a8bbf02dc58ca052cce
xavier21210 <117159696+xavier21210@users.noreply.github.com>
2024-03-28 13:21:02 +0100

    commit 7

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -10,18 +10,7 @@
     src="/img/image1.png"
     alt="phot de Béné"
   >
-  <div class="Text-content">
-    <h3>🌟 Bienvenue dans l'univers de la Beauté éclatante ! 🌟</h3>
-    <p> Hey les beautés en herbe ! Vous voilà dans l'antre de la beauté où les
-      astuces pétillantes et les conseils glamour sont à l'honneur. Chez nous, la
-      beauté rime avec fun, authenticité et éclat ! Préparez-vous à plonger dans
-      un monde où chaque produit raconte une histoire, où chaque conseil fait
-      briller votre journée et où chaque visite vous laisse avec un sourire
-      éblouissant. Restez connectés pour des astuces de maquillage, des conseils
-      de soins de la peau et des découvertes de produits qui vont transformer
-      votre routine beauté en une véritable fête pour les sens ! 💄✨ Prêts à
-      illuminer votre beauté ? Let’s glow together!</p>
-  </div>
+
 </div>
 
 
