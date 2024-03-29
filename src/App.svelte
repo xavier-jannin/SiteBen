6e749ff497d30b643e7f7184d6438a1407090b38
xavier21210 <117159696+xavier21210@users.noreply.github.com>
2024-03-29 11:36:50 +0100

    commit 15

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,9 @@
   import Header from "./components/Header.svelte";
   import Home from "./components/Home.svelte";
   import Footer from "./components/Footer.svelte";
+ 
+ 
+ 
   let routes = {
     // define your routes here
     "/": Header,
