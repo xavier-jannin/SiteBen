864d6158a749a12a9e58ba9b6b7417ac849a6ee7
xavier21210 <117159696+xavier21210@users.noreply.github.com>
2024-04-04 07:45:43 +0200

    mise place page nouveautées.

--- src/components/Nouveautees.svelte
+++ src/components/Nouveautees.svelte
@@ -5,11 +5,49 @@
 <h1> Les nouveautées</h1>
 
 <section class="Nouveautées">
-<div class="Nouveautées">
+    <div class="products">
+        <div class="product">
+            <img src="public\img\istockphoto-1473899426-1024x1024.jpg" alt="Chemise élégante">
+            <h3>Chemise élégante</h3>
+            <p>Prix : 49,99 €</p>
+        </div>
+        <!-- Ajoutez les 9 autres produits ici -->
 
+        <div class="product">
+            <img src="public\img\istockphoto-1473899426-1024x1024.jpg" alt="Chemise élégante">
+            <h3>Chemise élégante</h3>
+            <p>Prix : 49,99 €</p>
+        </div>
 
-</div>
+        <div class="product">
+            <img src="public\img\istockphoto-1473899426-1024x1024.jpg" alt="Chemise élégante">
+            <h3>Chemise élégante</h3>
+            <p>Prix : 49,99 €</p>
+        </div>
+
+        <div class="product">
+            <img src="public\img\istockphoto-1473899426-1024x1024.jpg" alt="Chemise élégante">
+            <h3>Chemise élégante</h3>
+            <p>Prix : 49,99 €</p>
+        </div>
+  
+        <div class="product">
+            <img src="public\img\istockphoto-1473899426-1024x1024.jpg" alt="Chemise élégante">
+            <h3>Chemise élégante</h3>
+            <p>Prix : 49,99 €</p>
+        </div>
+    
+        <div class="product">
+            <img src="public\img\istockphoto-1473899426-1024x1024.jpg" alt="Chemise élégante">
+            <h3>Chemise élégante</h3>
+            <p>Prix : 49,99 €</p>
+        </div>
+    
+    
+   
 </section>
+
+
 
 <style>
 /*Nouveautées*/
@@ -20,10 +58,32 @@ section.Nouveautées {
     justify-content: center;
     align-items: center;
     padding: 10px;
-
-
 }
 
+/*Produits*/
 
+/* Styles pour les écrans larges */
+.products {
+    display: flex;
+    flex-wrap: wrap;
+justify-content: center;
+align-items: center ;
+    
+}
+
+.product {
+    width: 30%;
+    margin: 10px;
+    padding: 10px;
+    border: 1px solid #ddd;
+}
+
+.product img {
+    
+    max-width: 100%;
+    height: auto;
+}
+
+/* Styles pour les écrans plus petits (par exemple, mobiles) */
 
 </style>
