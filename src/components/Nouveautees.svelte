8d8d365e81a1c1243083b3909397a13c8d3f335f
xavier21210 <117159696+xavier21210@users.noreply.github.com>
2024-04-04 11:38:25 +0200

    test image dans "Les nouveautées".

--- src/components/Nouveautees.svelte
+++ src/components/Nouveautees.svelte
@@ -14,25 +14,25 @@
         <!-- Ajoutez les 9 autres produits ici -->
 
         <div class="product">
-            <img src="/img/Ben.png" alt="Chemise élégante">
+            <img src="/img/image1.png" alt="Chemise élégante">
             <h3>Chemise élégante</h3>
             <p>Prix : 49,99 €</p>
         </div>
 
         <div class="product">
-            <img src="/img/Ben.png" alt="Chemise élégante">
+            <img src="/img/image0.png" alt="Chemise élégante">
             <h3>Chemise élégante</h3>
             <p>Prix : 49,99 €</p>
         </div>
 
         <div class="product">
-            <img src="/img/Ben.png" alt="Chemise élégante">
+            <img src="public\img\A26F910C-7A35-4BA7-B230-4F302F5B046F.PNG" alt="Chemise élégante">
             <h3>Chemise élégante</h3>
             <p>Prix : 49,99 €</p>
         </div>
   
         <div class="product">
-            <img src=" /img/Ben.png"alt="Chemise élégante">
+            <img src=" public\img\DADF9449-FC11-4A29-BA24-11A110FC89D9.JPG"alt="Chemise élégante">
             <h3>Chemise élégante</h3>
             <p>Prix : 49,99 €</p>
         </div>
